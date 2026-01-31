b8c40b38fd696e2f69bf8772e7d7a4f74dda0bc8
loganv308 <loganv308@gmail.com>
2026-01-30 19:18:28 -0600

    Enhance Runner class to extend Thread and implement OS-specific path handling. Introduce utils class for OS detection and improve media retrieval logic with retry mechanism.

--- src/main/java/com/loganv308/Runner.java
+++ src/main/java/com/loganv308/Runner.java
@@ -6,7 +6,7 @@ import java.util.List;
 // import io.github.cdimascio.dotenv.Dotenv;
 import java.util.Map;
 
-public class Runner {
+public class Runner extends Thread {
 
     // Grabs environment variables from .env file
     // private static final Dotenv dotenv = Dotenv.load();
@@ -18,19 +18,43 @@ public class Runner {
 
     private static FileScanner fs = new FileScanner();
 
+    private static utils ut = new utils();
+
     public static void main(String[] args) {
+        
+        Runner thread = new Runner();
+        
+        thread.start();
+
+        System.out.println("MediaFlow Thread is running...");
+
         while(true) {
             try {
-                String tempDir = "/tmp/nascopiestest/";
+
+                String tempDir = "";
+                Path nasRoot = Paths.get("");
+
+                // Determine OS and set paths accordingly
+                if(ut.getOS().contains("win")) {
+                    tempDir = "C:\\tmp\\nascopiestest\\";
+                    nasRoot = Paths.get("Y:\\movies");
+                // Else if OS == Linux or Mac
+                } else if(ut.getOS().contains("nix") || ut.getOS().contains("nux") || ut.getOS().contains("mac")) {
+                    tempDir = "/tmp/nascopiestest/";
+                    nasRoot = Paths.get("/mnt/NASMedia/movies");
+                }
 
                 System.out.println("Getting media...");
-
-                Path nasRoot = Paths.get("/mnt/NASMedia/movies");
 
                 Map<String, Path> nasIndex = fs.indexAllMedia(nasRoot.toString());
 
                 if (nasIndex.isEmpty()) {
-                    System.out.println("No media found.");
+                    System.out.println("No media found, retrying in 10 minutes...");
+                    try {
+                        Thread.sleep(600000); // Sleep for 10 minutes
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
                     return;
                 }
 
---
+++ src/main/java/com/loganv308/utils.java
@@ -0,0 +1,9 @@
+package com.loganv308;
+
+public class utils {
+    public String getOS() {
+        return System.getProperty("os.name").toLowerCase();
+    }
+
+    
+}
