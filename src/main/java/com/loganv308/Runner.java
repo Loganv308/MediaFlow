4129a7a7dd8ffb18bcbcadd528c538cb787b962b
loganv308 <loganv308@gmail.com>
2026-02-03 19:12:45 -0600

    Enhance Runner class with OS-specific path handling, including temp directory creation and NAS media path accessibility checks. Implement logging for files needing re-encoding and streamline media retrieval process.

--- src/main/java/com/loganv308/Runner.java
+++ src/main/java/com/loganv308/Runner.java
@@ -1,5 +1,6 @@
 package com.loganv308;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -30,14 +31,31 @@ public class Runner extends Thread {
 
         while(true) {
             try {
-
+                // Initialize variables
                 String tempDir = "";
                 Path nasRoot = Paths.get("");
 
                 // Determine OS and set paths accordingly
                 if(ut.getOS().contains("win")) {
+                    // Windows OS
                     tempDir = "C:\\tmp\\nascopiestest\\";
-                    nasRoot = Paths.get("Y:\\movies");
+
+                    // If the temp directory doesn't exist, create it
+                    if(!Files.exists(Paths.get(tempDir))) {
+                        Files.createDirectories(Paths.get(tempDir));
+                    }
+                    // Check if NAS media path is accessible 
+                    if(!Files.exists(Paths.get("Y:\\movies"))) {
+                        System.out.println("NAS Media path not found, retrying in 10 minutes...");
+                        try {
+                            Thread.sleep(600000); // Sleep for 10 minutes
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                        return;
+                    } else {
+                        nasRoot = Paths.get("Y:\\movies");
+                    }
                 // Else if OS == Linux or Mac
                 } else if(ut.getOS().contains("nix") || ut.getOS().contains("nux") || ut.getOS().contains("mac")) {
                     tempDir = "/tmp/nascopiestest/";
@@ -47,6 +65,8 @@ public class Runner extends Thread {
                 System.out.println("Getting media...");
 
                 Map<String, Path> nasIndex = fs.indexAllMedia(nasRoot.toString());
+
+                System.out.println(nasIndex);
 
                 if (nasIndex.isEmpty()) {
                     System.out.println("No media found, retrying in 10 minutes...");
@@ -58,26 +78,31 @@ public class Runner extends Thread {
                     return;
                 }
 
+                // Filters the index for files needing re-encoding
                 List<Path> needsReencode = nasIndex.values().stream()
                     .filter(Encoder::isWrongEncoding)
                     .filter(Encoder::isAbove1080p)
                     .toList();
 
-                System.out.println("Files needing re-encode: ");
+                // Logs number of files needing re-encode
+                System.out.println("Files needing re-encode: " + needsReencode.size());
                 
                 for(Path i : needsReencode) {
                     fs.copyOffNAS(i.toString(), tempDir);
                     System.out.println("Copy complete for: " + i.toString());
+                    System.out.println("Re-encoding for " + i.toString() + " in progress...");
+                    enc.reEncode(i.toString());
                 }
 
+                // Gets list of files in temp directory
                 List<Path> tempDirMediaList = fs.getTempPaths();
 
-                for(Path p : tempDirMediaList) {
-                    System.out.println("Re-encoding for " + p.toString() + " in progress...");
-                    enc.reEncode(p.toString());
-                }
-                
-                // TODO: Utilize outside classes to scan NAS for new and previous media, encode once found if needed. 
+                // Cleans up temp directory
+                // for(Path p : tempDirMediaList) {
+
+                // }
+
+
 
             } catch (Exception e) {
                 System.out.println(e);
